f9acf2f6c61581fb2223efa2ec20f3255795cfc2
Alexander Krylov <saskrkr123.com@mail.ru>
2025-12-19 18:39:29 +0300

    update 404 page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,13 +1,27 @@
-import './style/notfound.css'
 import Link from 'next/link'
-export default function notfount() {
-    return (
-        <div className='cont'>   
-            <div className='end'>
-                <h1>404</h1>
-                <Link style={{ textDecoration: 'none' }} href='/'>Вренуться обратно</Link>
-            </div>
-            
-        </div>  
-    )
+
+export default function NotFound() {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <div className="text-center">
+        <h1 className="text-9xl font-bold text-white-600">404</h1>
+        <h2 className="mt-4 text-3xl font-semibold text-white-800">
+          Страница не найдена
+        </h2>
+        <p className="mt-2 text-lg text-wgite-600">
+          Извините, запрашиваемая страница не существует.
+        </p>
+        <div className='flex justify-center'>
+          <Link
+          href="/"
+          className="items-center justify-center mt-8 w-45 h-10 flex rounded-lg bg-green-600 px-6 py-3 text-white 
+                     hover:bg-green-700 focus:outline-none focus:ring-2 
+                     focus:ring-green-500 focus:ring-offset-2 transition-colors"
+        >
+         <p> Вернуться на главную</p>
+        </Link>
+        </div>
+      </div>
+    </div>
+  )
 }
